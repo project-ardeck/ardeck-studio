9739f6e6350ee400cccfc00d5837a9e1939f5667
akurakuu <77902040+akurakuu@users.noreply.github.com>
2024-11-26 23:10:31 +0900

    delete

--- src-tauri/src/ardeck_studio/plugin.rs
+++ src-tauri/src/ardeck_studio/plugin.rs
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-pub mod config_singleton;
 pub mod core;
 pub mod manager;
 pub mod tauri;
