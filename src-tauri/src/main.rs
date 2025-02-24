a92d5d31d99ed03ea9679e2f9bca697fe1aed05e
akurakuu <77902040+akurakuu@users.noreply.github.com>
2025-02-25 03:29:40 +0900

    mainを修正

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,7 +19,9 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use ardeck::run;
+
 #[tokio::main]
 async fn main() {
-    app_lib::run().await;
+    run().await;
 }
